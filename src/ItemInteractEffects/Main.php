13a8326affc3aa2e97b90dd9d9aaba4180031e80
RumBenCraftPE <ameensahal321@gmail.com>
2016-07-29 20:20:59 +0300

    Fixed all known bugs.

--- src/ItemInteractEffects/Main.php
+++ src/ItemInteractEffects/Main.php
@@ -6,10 +6,11 @@ use pocketmine\plugin\PluginBase;
 use pocketmine\Player;
 use pocketmine\entity\Effect;
 use pocketmine\utils\Config;
+use pocketmine\utils\TextFormat;
 use pocketmine\event\Listener;
 class Main extends PluginBase implements Listener {
 	
-	####### ####### #######   ##
+	####### ####### #######   #
 	       #                #         #                 #
 	       #                #         #######
 	       #                #         #
@@ -18,14 +19,14 @@ class Main extends PluginBase implements Listener {
 	
 	private $config;
 	public function onLoad() {
-                $this->getLogger()->info(TEXTFORMAT::YELLOW . "ItemInteractEffects by KairusDS is loading...");
+                $this->getLogger()->info(TextFormat::YELLOW . "ItemInteractEffects by KairusDS is loading...");
        }
 
 	public function onEnable() {
 		@mkdir($this->getDataFolder());
-		$this->config = new Config($this->getDataFolder() . "config.yml", Config::YAML, array(280 => array("on-use-tip-message" => "§bThe power of the stick has been used!", "effect-id" => 8, "effect-duration" => 1, "effect-amplifier" => 1, "Turn-off-message" => "§b§lThe power of the stick has been turned off!", "visibility" => true)));
+		$this->config = new Config($this->getDataFolder() . "config.yml", Config::YAML, array(280 => array("on-use-tip-message" => "§bThe power of the stick has been used!", "effect-id" => 8, "effect-duration" => 1, "effect-amplifier" => 1, "Turn-off-message" => "§b§lThe power of the stick has been turned off!", "visibility" => false)));
 		$this->getServer()->getPluginManager()->registerEvents($this, $this);
-                $this->getLogger()->info(TEXTFORMAT::AQUA . "ItemInteractEffects" . TEXTFORMAT::YELLOW . "Successfully loaded!");
+                $this->getLogger()->info(TEXTFORMAT::AQUA . "ItemInteractEffects" . TEXTFORMAT::YELLOW . " Successfully loaded!");
 	}
 	
 	public function onInteract(PlayerInteractEvent $event) {
@@ -36,9 +37,9 @@ class Main extends PluginBase implements Listener {
 				switch($player->hasEffect($ic["effect-id"])) {
 					case false:
 					$ticks = $ic["effect-duration"] * 20;
-                                        $visible = $ic["visibility"]
+                                        $visible = $ic["visibility"];
 					$amplifier = $ic["effect-amplifier"];
-					$player->addEffect(Effect::getEffect($ic["effect-id"])->setDuration($ticks)->setAmplifier($amplifier)->setVisible->($visible));
+					$player->addEffect(Effect::getEffect($ic["effect-id"])->setDuration($ticks)->setAmplifier($amplifier)->setVisible($visible));
 					$player->sendTip($ic["on-use-tip-message"]);
 					break;
 					
